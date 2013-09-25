aa36ac0660cc509982da745dd9876a59f584fa46
Tobias Jaehnel <tjaehnel@gmail.com>
2013-09-25 14:43:03 +0100

    Replaced alert window by jGrowl notification.

--- page.php
+++ page.php
@@ -9,10 +9,12 @@ require_once PPMDIR.'/viewRegistration.php';
 <link type="text/css" href="css.php" rel="Stylesheet" />
 <link type="text/css" href="<?=BASEURL?>css/ui-ppm/jquery-ui-1.10.3.custom.css" rel="Stylesheet" />
 <link type="text/css" href="<?=BASEURL?>css/jquery.multiselect.css" rel="Stylesheet" />
+<link type="text/css" href="<?=BASEURL?>css/jquery.jgrowl.css" rel="Stylesheet" />
 <script type="text/javascript" src="<?=BASEURL?>js/Math.uuid.js"></script>
 <script type="text/javascript" src="<?=BASEURL?>js/jquery.js"></script>
 <script type="text/javascript" src="<?=BASEURL?>js/jquery-ui.custom.min.js"></script>
 <script type="text/javascript" src="<?=BASEURL?>js/jquery.multiselect.min.js"></script>
+<script type="text/javascript" src="<?=BASEURL?>js/jquery.jgrowl.min.js"></script>
 <script type="text/javascript" src="<?=BASEURL?>js/jsonRPC2php-stm.client.js"></script>
 <script type="text/javascript" src="<?=BASEURL?>js/simpleTaskManager.js"></script>
 <script type="text/javascript" src="js.php"></script>
