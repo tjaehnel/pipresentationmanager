80326767bfa76149104692c3223dd87b6e4e2413
Tobias Jaehnel <tjaehnel@gmail.com>
2013-10-01 17:52:31 +0100

    Auto cleanup tests

--- prepareTesting.php
+++ prepareTesting.php
@@ -3,19 +3,12 @@ $command = $_GET['command'];
 try {
 switch ($command) {
 	case "init":
-		if(file_exists('test'))
-			throw new Exception("Test directory exists - clean up first");
-		//exec("cp -r testenvironment test", $dummy, $res);
-		$res = recurse_copy('testenvironment', 'test');
-		if(!$res)
-			throw new Exception("Unable to copy directory");
-		echo "Successfully initialized test";
+		doInit();
+		echo '<p id="success">Successfully initialized test</p>';
 		break;
 	case "cleanup":
-		exec("rm -rf test", $dummy, $res);
-		if($res != 0)
-			throw new Exception("Unable to remove directory");
-		echo "Successfully cleaned up test";
+		doCleanup();
+		echo '<p id="success">Successfully cleaned up test</p>';
 		break;
 	default:
 		throw new Exception("Invalid command");
@@ -25,6 +18,21 @@ switch ($command) {
 	header($e->getMessage());
 	echo $e->getMessage();
 	
+}
+
+function doInit() {
+	if(file_exists('test'))
+		doCleanup();
+	if(file_exists('test'))
+		throw new Exception("Test directory exists - clean up first");
+	//exec("cp -r testenvironment test", $dummy, $res);
+	$res = recurse_copy('testenvironment', 'test');
+	if(!$res)
+		throw new Exception("Unable to copy directory");
+}
+
+function doCleanup() {
+	exec("rm -rf test", $dummy, $res);
 }
 
 function recurse_copy($src,$dst) {
