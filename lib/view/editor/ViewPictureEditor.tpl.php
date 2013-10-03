2c7aef9093b8dde1aafa57258eda4dfcc3020a5f
Tobias Jaehnel <tjaehnel@gmail.com>
2013-10-03 15:40:33 +0100

    Made enable/disable title edit more usable.

--- lib/view/editor/ViewPictureEditor.tpl.php
+++ lib/view/editor/ViewPictureEditor.tpl.php
@@ -1,4 +1,7 @@
-<h2><span>Picture: </span><span class="title"></span></h2>
+<h2><span>Picture: </span><span class="title">
+<span class="titletext"></span>
+<a href="" class="titleEditLink"><img alt="" src="" class="titleEditImg"></a></span>
+</h2>
 <a href="" id="selectPictureLink">Select Background</a>
 <div>
 <img class="previewImage" alt="preview" src=""/>
