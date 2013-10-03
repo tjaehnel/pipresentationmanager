6166c5a14a33f72659f294694e7a90b398942aad
Tobias Jaehnel <tjaehnel@gmail.com>
2013-10-03 16:15:11 +0100

    Separated title edit activate/deactivate buttons

--- lib/view/editor/ViewPictureEditor.tpl.php
+++ lib/view/editor/ViewPictureEditor.tpl.php
@@ -1,6 +1,7 @@
 <h2><span>Picture: </span><span class="title">
 <span class="titletext"></span>
-<a href="" class="titleEditLink"><img alt="" src="" class="titleEditImg"></a></span>
+<img alt="" src="<?=BASEURL?>img/edit.png" class="titleEditActivate"></span>
+<img alt="" src="<?=BASEURL?>img/OK.png" class="titleEditDeactivate"></span>
 </h2>
 <a href="" id="selectPictureLink">Select Background</a>
 <div>
